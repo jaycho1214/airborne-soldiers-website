e30e62e50de2f1f08c996f67dcbd1b4cb7aa5e0c
Jay Cho <jaycho1214@gmail.com>
2023-04-13 23:40:33 +0900

    setup fontawesome icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import type { AppProps } from 'next/app';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 import { UserProvider } from '@/contexts';
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
