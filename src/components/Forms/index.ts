9c79cb52a488de882efb3cf05913d8d6857b78d2
Jay Cho <jaycho1214@gmail.com>
2023-04-15 14:22:14 +0900

    organize components

---
+++ src/components/Forms/index.ts
@@ -0,0 +1,5 @@
+import StyledSelect, { StyledSelectProps } from './StyledSelect';
+import TextInput, { TextInputProps } from './TextInput';
+
+export { TextInput, StyledSelect };
+export type { StyledSelectProps, TextInputProps };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,9 @@
 import Layout, { LayoutProps } from './Layout';
 import Menu, { MenuProps } from './Menu';
 import Spinner, { SpinnerProps } from './Spinner';
-import StyledSelect, { StyledSelectProps } from './StyledSelect';
-import TextInput, { TextInputProps } from './TextInput';
 
 export * from './Buttons';
-export { Layout, TextInput, Spinner, StyledSelect, Menu };
-export type {
-  LayoutProps,
-  TextInputProps,
-  SpinnerProps,
-  StyledSelectProps,
-  MenuProps,
-};
+export * from './Forms';
+
+export { Layout, Spinner, Menu };
+export type { LayoutProps, SpinnerProps, MenuProps };
