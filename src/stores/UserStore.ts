daf59d98125537c16f2907a3e20dadae2b668cc2
Jay Cho <jaycho1214@gmail.com>
2023-04-15 16:36:21 +0900

    add new method: fetchUser

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,10 +1,13 @@
-import { Instance, types } from 'mobx-state-tree';
+import { Instance, cast, flow, types } from 'mobx-state-tree';
 import { IUser, User } from './User';
+import { DataSnapshot, child, get, ref } from 'firebase/database';
+import { database } from '@/api';
 
 export const UserStore = types
   .model('User', {
     user: types.maybeNull(User),
     loading: types.optional(types.boolean, true),
+    cachedUsers: types.array(User),
   })
   .actions((self) => ({
     setLoading(value: boolean) {
@@ -13,6 +16,19 @@ export const UserStore = types
     updateUser(newUser: IUser | null) {
       self.user = newUser;
     },
+    fetchUser: flow(function* (uid: string) {
+      const cachedRes = self.cachedUsers.find((u) => u.uid === uid);
+      if (cachedRes) return cachedRes;
+      const snapshot: DataSnapshot = yield get(
+        child(ref(database), `users/${uid}`),
+      );
+      if (snapshot.exists()) {
+        const newUser = User.create(snapshot.val());
+        self.cachedUsers = cast([...self.cachedUsers, newUser]);
+        return newUser;
+      }
+      return null;
+    }),
   }))
   .create({});
 
