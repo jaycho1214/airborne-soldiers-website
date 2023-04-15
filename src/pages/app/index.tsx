5fe2e68c70bd81a0b69c72b675c9cdd2542a90c5
Jay Cho <jaycho1214@gmail.com>
2023-04-15 14:00:31 +0900

    add app page

---
+++ src/pages/app/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '@/components';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function App() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/');
+  }, [router]);
+
+  return <Layout />;
+}
